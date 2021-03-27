9ea7afa431e40b0a699cd405a543930685d77643
Neil Henning <neil.henning@hey.com>
2021-03-27 17:18:57 +0000

    Stupid hex bug sigh.

--- testcrate/src/main.rs
+++ testcrate/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let a = if let Some(a) = option_env!("TEST_A") {
+    let a = if let Some(a) = option_env!("TEST") {
         a.parse::<i32>().unwrap()
     } else {
         1
     };
-    let b = if let Some(b) = option_env!("TEST_B") {
+    let b = if let Some(b) = option_env!("TEST_LOOOONNNNNNNGGGGGGGGGGGGGGG") {
         b.parse::<i32>().unwrap()
     } else {
         1
