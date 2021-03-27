5fc9160102742dba0976cfa2cbed41e39f4effd2
Neil Henning <neil.henning@hey.com>
2021-03-27 08:25:33 +0000

    Allow getting the value back out the cache in the build.rs.

--- testcrate/build.rs
+++ testcrate/build.rs
@@ -4,5 +4,6 @@ use envcache::EnvCache;
 
 fn main() {
     let mut envcache = EnvCache::new();
-    envcache.cache("TEST");
+    envcache.cache("TEST_A");
+    envcache.cache("TEST_B");
 }
--- testcrate/src/main.rs
+++ testcrate/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    std::process::exit(option_env!("TEST").is_none() as i32);
+    let a = if let Some(a) = option_env!("TEST_A") {
+        a.parse::<i32>().unwrap()
+    } else {
+        1
+    };
+    let b = if let Some(b) = option_env!("TEST_B") {
+        b.parse::<i32>().unwrap()
+    } else {
+        1
+    };
+
+    std::process::exit(a * b);
 }
